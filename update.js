dd9ea13e31966fdd77db82cebb601d422ed149c5
Chris Nojima <cnojima@gmail.com>
2019-02-22 08:01:46 -0800

    wip

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-process.env.LOG_LEVEL = 'LOG';
+
 global = {
   ...global,
   completedVolumes: [],
@@ -9,38 +9,38 @@ global = {
 };
 
 require('./src/util/init');
-const cli = require('command-line-args');
-const history = require('./out/history.json');
+const cli       = require('command-line-args');
+const cliConfig = require('./src/config/cli-base');
+const history   = require('./out/history.json');
+const options   = cli(cliConfig);
 
-const go = () => {
+const go = async () => {
   if (history.length > 0) {
+    let config;
     const url = history.shift();
 
+    console.log(`updating [ ${url} ]`.green);
+
     if (url.toLowerCase().indexOf('8muses') > -1) {
-      const options = cli(require('./src/config/8muses/cli'));
       options.url = url;
-      global.config = require('./src/config/8muses');
-      (async () => {await require('./src/8muses/start')(options); go()});
+      config = require('./src/config/8muses');
+      await require('./src/8muses/start')(options, config, '8muses');
     } else if (url.toLowerCase().indexOf('readcomicsonline') > -1) {
-      const options = cli(require('./src/config/rco/cli'));
       options.url = url;
-      global.config = require('./src/config/rco');
-      (async () => {await require('./src/rco/start')(options); go()});
+      config = require('./src/config/rco');
+      await require('./src/start')(options, config, 'rco');
     } else if (url.toLowerCase().indexOf('mangakakalot') > -1) {
-      const options = cli(require('./src/config/mangakakalot/cli'));
       options.url = url;
-      global.config = require('./src/config/mangakakalot');
-      (async () => {await require('./src/mangakakalot/start')(options); go()});
+      config = require('./src/config/mangakakalot');
+      await require('./src/start')(options, config, 'mangakakalot');
     } else if (url.toLowerCase().indexOf('mangareader') > -1) {
-      const options = cli(require('./src/config/mangareader/cli'));
       options.url = url;
-      global.config = require('./src/config/mangareader');
-      (async () => {await require('./src/mangareader/start')(options); go()});
+      config = require('./src/config/mangareader');
+      await require('./src/start')(options, config, 'mangareader');
     } else if (url.toLowerCase().indexOf('funmanga') > -1) {
-      const options = cli(require('./src/config/funmanga/cli'));
       options.url = url;
-      global.config = require('./src/config/funmanga');
-      (async () => {await require('./src/funmanga/start')(options); go()});
+      config = require('./src/config/funmanga');
+      await require('./src/start')(options, config, 'funmanga');
     }
   } else {
     console.log(`DONE`.green);
